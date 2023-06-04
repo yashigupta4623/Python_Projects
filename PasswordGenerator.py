fb1e646c6e1908c4b6c85dede438f91f2369327a
Yashi Gupta <98335217+yashigupta4623@users.noreply.github.com>
2023-06-04 15:31:01 +0530

    Update PasswordGenerator.py

---
+++ PasswordGenerator.py
@@ -0,0 +1,15 @@
+import random 
+
+letters =['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
+           'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
+           'S', 'T', 'U', 'V', 'W', 'X','Y','Z']
+
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+
+print("Welcome to the PassWord Generator!")
+
+no_letter = int(input("How many Letters would you like in your Password?\n"))
+no_symbols = int(input("How many Symbols would you like in your Password?\n"))
+no_numbers = int(input("How many Numbers would you like in your Password?\n"))
