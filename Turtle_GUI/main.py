425f3f96e22846821a707e92de7a8fb2ad84e106
Yashi Gupta <98335217+yashigupta4623@users.noreply.github.com>
2023-06-17 15:08:31 +0530

    Update main.py

--- Turtle_GUI/main.py
+++ Turtle_GUI/main.py
@@ -1,2 +1,38 @@
-import another_module
-import turtle
+import another_module #another_Variable = 20 --- written this in the file.
+
+# object.attribute
+# object.methods
+'''ATTRIBUTES -- features like car has speed, fuel etc
+METHODS -- function like def move(): speed = 60'''
+
+# import turtle #mandatory to write
+# timmy = turtle.Turtle() 
+# ''' is mandatory to write and "timmy" is just a
+# variable and Turtle is the class of the turtle method'''
+
+#OR
+
+from turtle import Turtle, Screen
+timmy = Turtle()
+print(timmy)
+timmy.shape("turtle") #to change the shape of the turtle
+timmy.color("green")
+timmy.forward(100)
+
+my_screen = Screen()
+print(my_screen.canvheight) # will display for milliseconds with height
+my_screen.exitonclick() #to pause the screen until we click
+
+
+
+
+
+
+
+
+
+
+
+
+
+
