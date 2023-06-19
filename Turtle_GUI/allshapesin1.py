7f8b349c70fb9c525f8040d3bbc8d515374cea38
Yashi Gupta <98335217+yashigupta4623@users.noreply.github.com>
2023-06-19 18:54:04 +0530

    Update allshapesin1.py

--- Turtle_GUI/allshapesin1.py
+++ Turtle_GUI/allshapesin1.py
@@ -1,1 +1,20 @@
+from turtle import Turtle, Screen 
+import random
 
+t = Turtle()
+
+colours = ["IndianRed","Yellow","Black","Blue","Green","Orange","pink"]
+
+def draw_angle(num_sides):
+    angle =  360/num_sides
+    for i in range(num_sides):
+        t.forward(100)
+        t.right(angle)
+
+for shape_side in range(3,10):
+    t.color(random.choice(colours))
+    draw_angle(shape_side)
+
+
+screen = Screen()
+screen.exitonclick()
