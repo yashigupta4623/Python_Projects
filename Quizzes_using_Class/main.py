bd6b1ead73baafbbf6e760f7cda5ee3d73a1ccaf
Yashi Gupta <98335217+yashigupta4623@users.noreply.github.com>
2023-06-19 14:13:56 +0530

    Update main.py

--- Quizzes_using_Class/main.py
+++ Quizzes_using_Class/main.py
@@ -3,4 +3,9 @@ from data import question_data
 
 question_bank = []
 for question in question_data:
-  
+  question_text = question['text'] 
+  question_answer = question['answer']
+  new_question = Question(question_text,question_answer)
+  question_bank.append(new_question)
+
+print(question_bank)
