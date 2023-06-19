7aeffed179ceb84f52423b2bb3c1d896e90ea07c
Yashi Gupta <98335217+yashigupta4623@users.noreply.github.com>
2023-06-19 15:00:13 +0530

    Update quiz_brain.py

--- Quizzes_using_Class/quiz_brain.py
+++ Quizzes_using_Class/quiz_brain.py
@@ -2,6 +2,13 @@ class QuizBrain:
   def __init__(self, q_list):
     self.question_number = 0
     self.question_list = q_list
+
+  def still_has_questions(self):
+    if self.question_number < len(self.question_list):
+      return True
+    else:
+      False
+  
   def next_question(self):
     current_question = self.question_list[self.question_list]
     self.question_number += 1
