4f7c846d2f9d91081f4e6051aca6cfa0dd2d3ab7
Yashi Gupta <98335217+yashigupta4623@users.noreply.github.com>
2023-06-22 15:14:57 +0530

    Create HigherOrderFunction.py

---
+++ Turtle_GUI/HigherOrderFunction.py
@@ -0,0 +1,11 @@
+from turtle import Turtle, Screen 
+
+tim = Turtle()
+screen = Screen() 
+
+def move_forward():
+  tim.forward(10) 
+  
+screen.listen() 
+screen.onkey(fun = move_forward, key = "space") 
+screen.exitonclick()
